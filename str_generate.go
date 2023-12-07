23649263e5fac67b3365d3c06fb0741f6ef6cc12
bdxing <353150280@qq.com>
2023-12-07 10:54:52 +0800

    feature: 注释

--- str_generate.go
+++ str_generate.go
@@ -4,7 +4,7 @@ import "math/rand"
 
 // StrGenerateRandom 从种子字符串里随机生成 n 个字符组成字符串。
 // 注意：math/rand 伪随机特性
-// 提前调用 rand.Seed(time.Now().UTC().UnixNano()) 初始化随机种子
+// 提前调用 rand.Seed(time.Now().UTC().UnixNano()) 初始化随机种子，可避免预判生成特征。
 func StrGenerateRandom(n int, allowedChar []rune) string {
 	b := make([]rune, n)
 	for i := range b {
